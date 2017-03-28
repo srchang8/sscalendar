cbd8192bdf61d8690f426b9fe7f05470230c1440
StephenChang <srchang8@gmail.com>
2017-03-28 14:57:24 -0400

    removed ds error

--- Example/Pods/Target Support Files/SSCalendar/SSCalendar-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SSCalendar : NSObject
-@end
-@implementation PodsDummy_SSCalendar
-@end
--- SSCalendar/Classes/Controllers/Month/ViewControllers/SSCalendarMonthlyViewController.m
+++ SSCalendar/Classes/Controllers/Month/ViewControllers/SSCalendarMonthlyViewController.m
@@ -4,8 +4,8 @@
 //
 //  Created by Steven Preston on 7/24/13.
 //  Copyright (c) 2013 Stellar16. All rights reserved.
-//test for commit
-ds
+//hello
+
 
 #import "SSCalendarMonthlyViewController.h"
 #import "SSCalendarMonthlyDataSource.h"
